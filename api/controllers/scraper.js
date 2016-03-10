d3346f5faa8e45b42c89bc8e693c7c462f6a8bb0
johntangma <johntangma@gmail.com>
2016-03-10 19:40:17 +0800

    coding style change

--- api/controllers/scraper.js
+++ api/controllers/scraper.js
@@ -12,16 +12,20 @@ export async function getJackpot(req, res, next){
     if(!selected) {
         return res.send({message: "failed to read data"});
     }
+
     const jackpot = selected.match(/\d/g).join("");
+    const stringPart = selected.substr(selected.indexOf(jackpot) + 3);
     let multiplier;
-    if(selected.indexOf("Thousand") > -1) {
-        multiplier = Math.pow(10, 3);
-    }
-    else if(selected.indexOf("Million") > -1){
-        multiplier = Math.pow(10, 6);
-    }
-    else if(selected.indexOf("Billion") > -1) {
-        multiplier = Math.pow(10, 9);
+    switch(stringPart){
+        case "Thousand":
+            multiplier = Math.pow(10, 3);
+            break;
+        case "Million":
+            multiplier = Math.pow(10, 6);
+            break;
+        case "Billion":
+            multiplier = Math.pow(10, 9);
+            break;
     }
 
     const jackpotInCents = jackpot * multiplier * Math.pow(10,2);
@@ -30,10 +34,6 @@ export async function getJackpot(req, res, next){
 
 export async function getWinningNumbers(req, res, next){
     const html = await request(WINNUMS_URL);
-
-    if(!html) {
-        return res.send({message: "failed to read data"});
-    }
     const latestLine = html.split("\n")[1].trim();
 
     let winningNumbers = latestLine.split("  ").splice(1);
