e98eb6b1e77ba68d5197d413de2733067480e9f5
johntangma <johntangma@gmail.com>
2016-03-10 18:07:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scraper
+
+### How to run locally?
+
+1. Clone git repository to your local directory.
+
+2. Go to your local directory on console(terminal) and run `npm install`
+
+3. `npm start`
---
+++ api/controllers/scraper.js
@@ -0,0 +1,33 @@
+import cheerio from "cheerio";
+import request from "request-promise";
+
+export async function getJackpot(req, res, next){
+    const html = await request("http://www.powerball.com/pb_home.asp");
+    const $ = cheerio.load(html);
+    const selected = $("div#mainContent table").eq(0).find("tr").eq(1).find("td").last().text().trim();
+
+    const jackpot = selected.match(/\d/g).join("");
+    let multiplier;
+    if(selected.indexOf("Thousand") > -1) {
+        multiplier = Math.pow(10, 3);
+    }
+    else if(selected.indexOf("Million") > -1){
+        multiplier = Math.pow(10, 6);
+    }
+    else if(selected.indexOf("Billion") > -1) {
+        multiplier = Math.pow(10, 9);
+    }
+
+    const jackpotInCents = jackpot * multiplier * Math.pow(10,3);
+    return res.send({ data: jackpotInCents });
+}
+
+export async function getWinningNumbers(req, res, next){
+    const html = await request("http://www.powerball.com/powerball/winnums-text.txt");
+    const latestLine = html.split("\n")[1].trim();
+
+    let winningNumbers = latestLine.split("  ").splice(1);
+    winningNumbers = winningNumbers.map(number => parseInt(number));
+
+    return res.send({ data: winningNumbers });
+}
